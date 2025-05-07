f2528233db338768751f1fc6124ca7b1c2a10233
reactjs-bootstrap <147966324+reactjs-bootstrap@users.noreply.github.com>
2025-05-07 14:18:24 +0530

    Update UpdateTask.jsx

--- src/UpdateTask.jsx
+++ src/UpdateTask.jsx
@@ -27,8 +27,8 @@ const UpdateTask = () => {
   }, []);
 
   return (
-    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-      <div className="w-50 bg-white rounded p-3">
+    <div className="d-flex justify-content-start align-items-start">
+      <div className="bg-white rounded p-3">
         <form onSubmit={handleSubmit}>
           <h2>Update TaskList</h2>
           <div className="mb-2">
