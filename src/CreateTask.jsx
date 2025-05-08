a9da0717a5b9daeab426fca616bdca9e5c4208f6
reactjs-bootstrap <147966324+reactjs-bootstrap@users.noreply.github.com>
2025-05-08 13:12:21 +0530

    Update CreateTask.jsx

--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -31,59 +31,57 @@ const CreateTask = () => {
 
   return (
     <div className="d-flex justify-content-start align-items-start">
-<<<<<<< HEAD
       <div className="bg-white w-100 rounded p-3">
-=======
-      <div className="w-100 bg-white rounded p-3">
->>>>>>> eeb37ff5aea38fbf7068785c2d097a9154964f94
-        <form onSubmit={handleSubmit}>
-          <h2>Add Task</h2>
+        <div className="w-100 bg-white rounded p-3">
+          <form onSubmit={handleSubmit}>
+            <h2>Add Task</h2>
 
-          <div className="mb-2">
-            <label>Task Details</label>
-            <input
-              type="text"
-              placeholder="Enter Task Details"
-              className="form-control shadow-none"
-              value={taskName
-                .toLowerCase()
-                .split(" ")
-                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(" ")}
-              onChange={(e) => setTaskName(e.target.value)}
-            />
-          </div>
+            <div className="mb-2">
+              <label>Task Details</label>
+              <input
+                type="text"
+                placeholder="Enter Task Details"
+                className="form-control shadow-none"
+                value={taskName
+                  .toLowerCase()
+                  .split(" ")
+                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                  .join(" ")}
+                onChange={(e) => setTaskName(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-2  ">
-            <label>Status</label>
-            <select
-              className="form-select shadow-none"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-            >
-              {/* <option value="">Select Status</option> */}
-              <option value="Pending">Pending</option>
-              <option value="Started">Started</option>
-              <option value="Completed">Completed</option>
-            </select>
-          </div>
+            <div className="mb-2  ">
+              <label>Status</label>
+              <select
+                className="form-select shadow-none"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                {/* <option value="">Select Status</option> */}
+                <option value="Pending">Pending</option>
+                <option value="Started">Started</option>
+                <option value="Completed">Completed</option>
+              </select>
+            </div>
 
-          <button className="btn btn-success ms-2" type="submit">
-            Submit
-          </button>
-          <button
-            type="button"
-            className="btn btn-success ms-2"
-            onClick={handleReset}
-          >
-            Reset
-          </button>
-          <Link to="/">
-            <button type="button" className="btn btn-success ms-2">
-              Home
+            <button className="btn btn-success ms-2" type="submit">
+              Submit
             </button>
-          </Link>
-        </form>
+            <button
+              type="button"
+              className="btn btn-success ms-2"
+              onClick={handleReset}
+            >
+              Reset
+            </button>
+            <Link to="/">
+              <button type="button" className="btn btn-success ms-2">
+                Home
+              </button>
+            </Link>
+          </form>
+        </div>
       </div>
     </div>
   );
